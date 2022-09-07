b1113cc3bbe3ffa75b97e1ccf818614752ad1863
Calvin-C-M <montolalu28@gmail.com>
2022-09-07 21:44:56 +0700

    Fix: UI hotfix for menu

--- Shift Cipher/main.cpp
+++ Shift Cipher/main.cpp
@@ -9,24 +9,24 @@ int main() {
         int key=1;
         int pilihan=1;
 
-        printf("MENU");
-        printf("1. Enkripsi");
-        printf("2. Dekripsi");
-        printf("3. Keluar");
+        printf("\nMENU\n");
+        printf("1. Enkripsi\n");
+        printf("2. Dekripsi\n");
+        printf("3. Keluar\n");
 
         std::cout << "Pilihan: "; std::cin >> pilihan;
 
         switch(pilihan) {
             case 1:
-                std::cout << "Masukkan teks yang ingin dienkripsi: ";
+                std::cout << "Masukkan teks yang ingin dienkripsi: "; std::cin >> plaintext;
                 std::cout << "Masukkan key: "; std::cin >> key;
-                std::cout << "Hasil enkripsi = " << ShiftCipher::encrypt(plaintext,key);
+                std::cout << "Hasil enkripsi = " << ShiftCipher::encrypt(plaintext,key) << "\n";
                 break;
 
             case 2:
-                std::cout << "Masukkan teks yang ingin didekripsi: ";
+                std::cout << "Masukkan teks yang ingin didekripsi: "; std::cin >> plaintext;
                 std::cout << "Masukkan key: "; std::cin >> key;
-                std::cout << "Hasil dekripsi = " << ShiftCipher::decrypt(plaintext,key);
+                std::cout << "Hasil dekripsi = " << ShiftCipher::decrypt(plaintext,key) << "\n";
                 break;
 
             case 3:
@@ -34,7 +34,8 @@ int main() {
                 break;
 
             default: 
-                printf("Ada kesalahan dalam memilih, silahkan coba lagi!");
+                printf("Ada kesalahan dalam memilih, silahkan coba lagi!\n");
+                system("pause");
                 break;
         }
     }
