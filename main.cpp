09069bed2a71cc5ba7fe0c7415f5cb889c67b389
Calvin-C-M <montolalu28@gmail.com>
2022-09-14 23:21:45 +0700

    Added group members name

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+/**
+ * KELOMPOK 4
+ * 
+ * Anggota
+ * 1. Alfadli Maulana Siddik    (140810200005)
+ * 2. Wafi Fahruzzahman         (140810200009)
+ * 3. Naufal Fahrezi            (140810200035)
+ * 4. Calvin Calfi Montolalu    (140810200053)
+ * 5. Deani Puteri Virdiana     (140810200059)
+ * 
+ */
+
 #include <iostream>
 #include "shift.cpp"
 #include "affine.cpp"
